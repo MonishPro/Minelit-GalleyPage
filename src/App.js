f8a13f51573d3c18b4a411e79f196d4eb74bb8d5
AmanPathan <amanpathan526@gmail.com>
2023-05-01 03:18:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Gallery from './components/Gallery';
+import Main from './components/Main';
+
+function App() {
+  return (
+    <>
+      
+
+      <Gallery />
+      <Main/>
+    </>
+  );
+}
+
+export default App;
